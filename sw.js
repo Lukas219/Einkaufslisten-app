c078d4e0decdcea2b4a1c8e2bc3aff0078accabf
Lukas <hrz225wfgy@privaterelay.appleid.com>
2026-09-09 14:56:05 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,80 +1,41 @@
-const CACHE_NAME = "einkaufslisten-v7";
+const CACHE_NAME = "einkaufslisten-v8";
 
 const DATEIEN = [
     "./",
     "./index.html",
-    "./manifest.json"
+    "./manifest.json",
+    "./icons/icon-192.png",
+    "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
-
     event.waitUntil(
-
-        caches.open(CACHE_NAME)
-            .then(cache => {
-
-                return cache.addAll(
-                    DATEIEN
-                );
-
-            })
-
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(DATEIEN);
+        })
     );
 
+    self.skipWaiting();
 });
-
 
 self.addEventListener("activate", event => {
-
     event.waitUntil(
-
-        caches.keys()
-            .then(cacheNames => {
-
-                return Promise.all(
-
-                    cacheNames
-                        .filter(
-                            name =>
-                                name !== CACHE_NAME
-                        )
-                        .map(
-                            name =>
-                                caches.delete(
-                                    name
-                                )
-                        )
-
-                );
-
-            })
-
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames
+                    .filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            );
+        })
     );
 
+    self.clients.claim();
 });
 
-
 self.addEventListener("fetch", event => {
-
     event.respondWith(
-
-        caches.match(
-            event.request
-        )
-        .then(response => {
-
-            if (response) {
-
-                return response;
-
-            }
-
-            return fetch(
-                event.request
-            );
-
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
         })
-
     );
-
 });
